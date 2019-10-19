353b1dc3128325b92a690af6968103f6315663ac
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-19 17:16:10 +0200

    Update txt_to_pdf.py

--- txt_to_pdf.py
+++ txt_to_pdf.py
@@ -1,18 +1,25 @@
 #PASAR ARCHIVO ".txt" A PDF
 from fpdf import FPDF
 
+#ABRIR FICHERO A CONVERTIR
+fichero = open("prueba.txt","r")
+
+#OBJETO "pdf"
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial",size=10)
 line=1
 
-fichero = open("prueba.txt","r")
-
+#LECTURA ".txt"/ESCRITURA ".pdf"
 for linea in fichero:
     pdf.cell(200,7,txt=linea,ln=line,align="L")
     if linea[-1]==("\n"):
         linea=linea[:-1]
     line+=1
 
-pdf.output("prueba5.pdf")
+#CREAR PDF
+pdf.output("prueba2.pdf")
+
+#CERRAR FICHERO.
 fichero.close()
+
